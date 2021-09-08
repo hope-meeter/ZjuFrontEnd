91ed70292ef169f176a5b5bf48dda126dd8c3edb
hope-meeter <56471909+hope-meeter@users.noreply.github.com>
2021-09-08 21:52:52 +0800

    2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,19 @@
 <template>
-	<div>成功提高了网管的抗压能力（此页待更新）</div>
-<!-- 	<div>更新日志</div>
+	<div>
+		<span>
+			一起改进网站！
+		</span>
+		<a href="https://github.com/hope-meeter/ZjuFrontEnd/tree/master">
+			<img src="https://img.shields.io/badge/github-ZjuFrontEnd-brightgreen.svg" alt="github">
+		</a>
+		<div>
+			<span>
+				网管的评价是：网站成立以来，成功提高了网管的抗压能力（此页待更新）
+			</span>
+		</div>
+		<!-- 	<div>更新日志</div>
 	<div>20210908 防垃圾、全站HTTPS</div> -->
+	</div>
 </template>
 
 <script>
